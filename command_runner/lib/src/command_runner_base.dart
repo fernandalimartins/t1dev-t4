0c8be17159f067c1ab7c7ffaabb271eb15eaddc4
Fernanda Marques <fernanda.d@edu.senai.br>
2026-06-11 16:16:00 +0000

    Lição 08 - Tarefa 02 - Adicionar um onOutputretorno de chamada

--- command_runner/lib/src/command_runner_base.dart
+++ command_runner/lib/src/command_runner_base.dart
@@ -6,6 +6,15 @@ import 'arguments.dart';
 import 'exceptions.dart';
 
 class CommandRunner {
+  CommandRunner({this.onOutput, this.onError});
+
+  /// If not null, this method is used to handle output. Useful if you want to
+  /// execute code before the output is printed to the console, or if you
+  /// want to do something other than print output the console.
+  /// If null, the onInput method will [print] the output.
+  FutureOr<void> Function(String)? onOutput;
+
+  FutureOr<void> Function(Object)? onError;
   // Construtor aceitando o callback opcional de erro
   CommandRunner({this.onError});
 
@@ -17,13 +26,17 @@ class CommandRunner {
   // Propriedade onError definida corretamente
   FutureOr<void> Function(Object)? onError;
 
-  // Método run unificado com o bloco try-catch correto
+
   Future<void> run(List<String> input) async {
     try {
       final ArgResults results = parse(input);
       if (results.command != null) {
         Object? output = await results.command!.run(results);
-        print(output.toString());
+        if (onOutput != null) {
+          await onOutput!(output.toString());
+        } else {
+          print(output.toString());
+        }
       }
     } on Exception catch (exception) {
       if (onError != null) {
@@ -33,6 +46,7 @@ class CommandRunner {
       }
     }
   }
+
 
   void addCommand(Command command) {
     // TODO: handle error (Commands can't have names that conflict)
