b247a40d115d8a9ecaabe91651ad03b9dc4bdf5b
Fernanda Lima <fernanda.l.martins6@aluno.senai.br>
2026-07-15 16:23:06 +0000

    Lição 12 - Tarefa 6 - Execute o aplicativo e verifique os registros.

--- cli/lib/src/commands/get_article.dart
+++ cli/lib/src/commands/get_article.dart
@@ -50,3 +50,4 @@ class GetArticleCommand extends Command {
     }
   }
 }
+
--- cli/lib/src/logger.dart
+++ cli/lib/src/logger.dart
@@ -2,17 +2,6 @@ import 'dart:io';
 import 'package:logging/logging.dart';
 
 Logger initFileLogger(String name) {
-  // Enables logging from child loggers.
-  hierarchicalLoggingEnabled = true;
-
-  // Create a logger instance with the provided name.
-  final logger = Logger(name);
-  final now = DateTime.now();
-
-  // The rest of the function will be added below.
-  // ...
-
-Logger initFileLogger(String name) {
   hierarchicalLoggingEnabled = true;
   final logger = Logger(name);
   final now = DateTime.now();
@@ -33,10 +22,7 @@ Logger initFileLogger(String name) {
 
   // The rest of the function will be added below.
   // ...
-
-  // Set the logger level to ALL, so it logs all messages regardless of severity.
-  // Level.ALL is useful for development and debugging, but you'll likely want to
-  // use a more restrictive level like Level.INFO or Level.WARNING in production.
+// Set the logger level to ALL, so it logs all messages regardless of severity.
   logger.level = Level.ALL;
 
   // Listen for log records and write each one to the log file.
@@ -48,4 +34,4 @@ Logger initFileLogger(String name) {
 
   return logger;
 }
-
+ 
